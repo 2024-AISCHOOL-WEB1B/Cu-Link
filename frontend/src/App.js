2df30e2cbb3c197bc8290e03563c8de10bc8c6a1
문종호 <jhmoonlight.nanum@gmail.com>
2024-10-15 15:43:50 +0900

    컴포넌트 밑 페이지 연결
    컴포넌트 밑 페이지 연결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./page/Main";
+import Baner from "./components/Baner";
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
     <div>
+      <Baner />
+      <SearchBar />
+      <Routes>
+        <Route path="/" element={<Main></Main>}></Route>
+      </Routes>
       <Main />
     </div>
   );
